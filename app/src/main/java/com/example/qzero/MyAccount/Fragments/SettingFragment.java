9614bd59de38cc1efa0f336eb09a3790d823ce37
Ashish Tiwari <ashish@braintechnosys.com>
2015-07-31 11:03:59 +0530

    Committing SettingFragment, layout and string resources

---
+++ app/src/main/java/com/example/qzero/MyAccount/Fragments/SettingFragment.java
@@ -0,0 +1,25 @@
+package com.example.qzero.MyAccount.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.qzero.R;
+
+/**
+ * Created by Braintech on 7/31/2015.
+ */
+public class SettingFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting,null);
+
+        return view;
+
+
+    }
+}
