d7ee80ed71fab55ce86d1c2683061b5aa705cf1e
braintech-neha <neha.tyagi@braintechnosys.biz>
2015-10-12 10:53:51 +0530

    pay pal integrated

--- app/src/main/java/com/example/qzero/Outlet/Fragments/PickupTabFragment.java
+++ app/src/main/java/com/example/qzero/Outlet/Fragments/PickupTabFragment.java
@@ -1,20 +1,50 @@
 package com.example.qzero.Outlet.Fragments;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.qzero.R;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 public class PickupTabFragment extends Fragment {
+
+    //set the environment for production/sandbox/no netowrk
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+
+    private static final String CONFIG_CLIENT_ID = "ASEg2Vc9lKh1QephLG7NYj4kzcL6MuMzo4GIGeMM5zqaDjxYwtliRgJkxnZx6utGsSfb81Kok3atIvR4";
+
+    private static final int REQUEST_PAYPAL_PAYMENT = 1;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+                    // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("QZERO");
+
 
 
     @Override
@@ -26,7 +56,57 @@ public class PickupTabFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Intent intent = new Intent(getActivity(), PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        getActivity().startService(intent);
+    }
+
     @OnClick(R.id.btn_PlaceOrder)void placeOrder(){
+
+        PayPalPayment thingToBuy = new PayPalPayment(new BigDecimal(5.00), "USD", "QZERO",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(getActivity(), PaymentActivity.class);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+
+        startActivityForResult(intent, REQUEST_PAYPAL_PAYMENT);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PAYPAL_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm = data
+                        .getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println("Responseeee" + confirm);
+                        Log.i("paymentExample", confirm.toJSONObject().toString());
+
+
+                        JSONObject jsonObj = new JSONObject(confirm.toJSONObject().toString());
+
+                        String paymentId = jsonObj.getJSONObject("response").getString("id");
+                        System.out.println("payment id:-==" + paymentId);
+                        Toast.makeText(getActivity(), paymentId, Toast.LENGTH_LONG).show();
+
+                    } catch (JSONException e) {
+                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("paymentExample", "An invalid Payment was submitted. Please see the docs.");
+            }
+        }
+
 
     }
 
