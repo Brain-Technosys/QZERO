504e0ff13e3023fb2b0adaf78b90615e1f18a7c6
Neha Tyagi <neha.tyagi@braintechnosys.biz>
2016-01-05 16:38:12 +0530

    search changed

--- app/src/main/java/com/example/qzero/Outlet/Activities/SearchVenueActivity.java
+++ app/src/main/java/com/example/qzero/Outlet/Activities/SearchVenueActivity.java
@@ -264,6 +264,8 @@ public class SearchVenueActivity extends Activity {
             String url = Const.BASE_URL + Const.GET_ITEMS + venue_id + "/?outletId=" + outletId + "&itemId=" +itemId
                     + "&subCatId=" +subCatId;
 
+            Log.e("url",url);
+
 
             String jsonString = jsonParser.getJSONFromUrl(url, Const.TIME_OUT);
 
